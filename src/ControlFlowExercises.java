b365b47cac1798a44d6d77bf1f11d9dbf2afab22
faithgarza <faith.garza001@gmail.com>
2020-01-28 20:38:27 -0600

    progress

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 Loop Basics
 
@@ -12,6 +14,7 @@ Your output should look like this:
 */
 class WhileLoopExample {
     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         int i = 5;
         do {
             System.out.println(i);
@@ -23,32 +26,33 @@ class WhileLoopExample {
 
  Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.*/
 
-                            int u = 2;
-                    do {
-
-                        System.out.println(u);
-                        u++;
-                    }
-                    while (u <= 100);
-
-
-/*Completed Alter your loop to count backwards by 5's from 100 to -10.*/
-        int p;
-
-        for ( p=100; p>=0; p=p-5 )                       // x = x - 5 is same as x -=5
-            System.out.println(p);
-
-
-/*Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal*/
-        int num = 2;
-        int z = 0; // loop variable initialization
-        do {
-            int value = num * z;
-            System.out.println(num + " * " + z + " = " + value);
-            z++;
-        } while ( z <= 100 ); // condition check for exit
+        int counter = 0;
+        for (const input of inputs) {
+            if (input % 2 === 0) counter += 1;
 
 
 
-    }
-}
+
+            /*Completed Alter your loop to count backwards by 5's from 100 to -10.*/
+            int p;
+
+            for (p = 100; p >= 0; p = p - 5)                       // x = x - 5 is same as x -=5
+                System.out.println(p);
+
+
+            /*Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal*/
+            int num = 2;
+            int z = 0; // loop variable initialization
+            do {
+                int value = num * z;
+                System.out.println(num + " * " + z + " = " + value);
+                z++;
+            } while (z <= 100); // condition check for exit
+
+        }
+
+
+
+
+
+
