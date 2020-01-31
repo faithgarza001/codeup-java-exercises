cd6dd64884e13643bfb4837ac88c87ecba2060b2
faithgarza <faith.garza001@gmail.com>
2020-01-31 15:27:10 -0600

    progress

---
+++ src/shapes/Circle.java
@@ -0,0 +1,28 @@
+package shapes;
+import java.util.Scanner;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+
+        this.radius = radius;
+    }
+
+    public  double getArea(){
+        return
+    }
+
+        int r;
+        double pi = 3.14, area;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter radius of circle:");
+        r = s.nextInt();
+        area = pi * r * r;
+        System.out.println("Area of circle:"+area);
+    }
+
+
+}
+
---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,9 @@
+package shapes;
+import util.input;
+
+public class CircleApp {
+
+    public static void main(String[] args){
+        Input input = new Input();
+    }
+}
