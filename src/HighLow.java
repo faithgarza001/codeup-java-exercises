e90b475819fe524347cb29de5d887e65bc086984
faithgarza <faith.garza001@gmail.com>
2020-01-30 23:26:49 -0600

    dialogue good

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,33 +1,68 @@
 import java.lang.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class HighLow {
-    public static void main(String args[]) {
-        game();
+    public static void main(String[] args) {
+        HighLowGame();
+
+
 
     }
 
-        public static void game () {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Would you like to play a game?");
-            String userInput = scanner.next();
-            String name = scanner.next();
-            if(userInput.equalsIgnoreCase("yes")) {
-                System.out.print("What is your name?");
-//                String name = scanner.next();//In here this is a local scope, in other words it is within the if statement only.
-                System.out.println("Hello, " + name + " Pleased to meet you!");
 
-            }else{
-                System.out.print("Sorry you don't want to play, come back sometime" + name);
+    public static void HighLowGame() {
+        //Ask if the user is ready to start. If they type in "yes", start the game
+        ArrayList = [1,2,3];
+        Integer a = null;
+        Integer b = null;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name?\n");
+
+        String name = scanner.next();//In here this is a local scope, in other words it is within the if statement only.
+
+        System.out.println("Hello, " + name + " Pleased to meet you!");
+
+        System.out.printf("Would you like to play a game?\n");
+
+        String userInput = scanner.next();
+
+
+        if (userInput.equalsIgnoreCase("yes")) {
+
+            System.out.print("Your Weapon is a function blaster with 2 bullet types code and hack");
+            System.out.println("You are at the planet of the code");
+            System.out.println("What do you want to do?");
+            System.out.println("a: Talk to the computer");
+            System.out.println("b: Attack the computer");
+            System.out.println(": Leave");
+            String youDecide = scanner.next();
+
+
+
+
+            if (youDecide.equalsIgnoreCase(1) {
+
+            } else if (youDecide.equals(2)) {
+
             }
+            {
 
-
-
-
+            } else if (youDecide.equals(3)) {
+                System.out.print("I must make this game mor interesting!");
+            } else if (youDecide.isEmpty()); {
+                System.out.print("Sorry to see you go");
+            }
         }
 
+
     }
+}
+
+
+
 
 
 
