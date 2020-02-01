370e8ed194e72e36e237005a08f34fd8a943bafd
faithgarza <faith.garza001@gmail.com>
2020-02-01 14:36:22 -0600

    progress

--- src/HighLow.java
+++ src/HighLow.java
@@ -11,20 +11,26 @@ public class HighLow {
         Scanner object that takes System.in as a parameter setting the variable of keyboard with the keyboard variable now we can use the other methods that are available in the scanner class.*/
 
         String Go, Look, Pantry, Eat;//Here a string of array (String []) which lies within the parameters.
-
+        int playerHP = 300;
+        int alienPoints = 400;
         System.out.println(" Welcome to the Europa ADVENTURE");//This is simply a print that is a string
         System.out.println("  ");//
-        System.out.println(" You have been exploring around a few days now you are in a the caves hanging from a zipLine, Would you like to go 'further up' or 'down'? ");
+        System.out.println("There are couple of things you need to know first your health is set at " + playerHP + " your nemesis alien bug is set at " + alienPoints);
+        System.out.println(" You have been exploring around a few days now you are in a the cave hanging from a zipLine, Would you like to go 'further up' or 'down'? ");
         System.out.print("> ");
         Go = keyboard.next();
         //the code above is assigning the String Go from the array
-        if (Go.equalsIgnoreCase("down")) {
-            System.out.println("There is a long countertop with dirty dishes everywhere. Off to one side there is, as you'd expect, a refrigerator. You may open the 'refrigerator' or look in the 'pantry'. ");
+        if (Go.equalsIgnoreCase("down")) {//here go equals down see 'down'
+
+            System.out.println("There are alien bugs everywhere. They are headed to the main server, you may try to kill them off with 'blaster' or look in the 'secret chest'. ");
             System.out.print(">  ");
             Look = keyboard.next();
 
-            if (Look.equalsIgnoreCase("refrigerator")) {
-                System.out.println("Inside the refrigerator you see food and stuff. It looks pretty nasty. Would you like to eat some of the food, 'Yes' or 'No'?");
+            if (Look.equalsIgnoreCase("blaster")) {
+                System.out.print("Please enter a javascript function that adds numbers 3 and 5 and returns the sum, do this in es6");
+                //request input from the user;
+
+                System.out.println("After killing off some of the bugs, you have used the battery life and need to wait for it to recharge. Would you like to eat some of the food, 'Yes' or 'No'?");
                 System.out.print(">  ");
                 Eat = keyboard.next();
 
