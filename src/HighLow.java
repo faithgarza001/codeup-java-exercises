103538a0b55024c9eb073a2c4d48c75c49bf9022
faithgarza <faith.garza001@gmail.com>
2020-02-01 15:45:13 -0600

    added more functionality

--- src/HighLow.java
+++ src/HighLow.java
@@ -10,25 +10,47 @@ public class HighLow {
         Scanner keyboard = new Scanner(System.in);/*“Scanner keyboard = new Scanner(System.in);” creates a
         Scanner object that takes System.in as a parameter setting the variable of keyboard with the keyboard variable now we can use the other methods that are available in the scanner class.*/
 
-        String Go, Look, Pantry, Eat;//Here a string of array (String []) which lies within the parameters.
+        String Go, Blast, Test, Pantry, Eat;//Here a string of array (String []) which lies within the parameters.
         int playerHP = 300;
         int alienPoints = 400;
         System.out.println(" Welcome to the Europa ADVENTURE");//This is simply a print that is a string
-        System.out.println("  ");//
-        System.out.println("There are couple of things you need to know first your health is set at " + playerHP + " your nemesis alien bug is set at " + alienPoints);
+        System.out.println("  ");//A line between lines printed when code is run.
+        System.out.println("There are couple of things you need to know first your health is set at " + playerHP + " your nemesis alien bug is set at " + alienPoints);//String plus int variables concat.
+        System.out.println("  ");//A line between lines printed when code is run.
         System.out.println(" You have been exploring around a few days now you are in a the cave hanging from a zipLine, Would you like to go 'further up' or 'down'? ");
         System.out.print("> ");
         Go = keyboard.next();
         //the code above is assigning the String Go from the array
-        if (Go.equalsIgnoreCase("down")) {//here go equals down see 'down'
+        if (Go.equalsIgnoreCase("down")) {//here go equals down see 'down' equalsIgnoreCase
 
             System.out.println("There are alien bugs everywhere. They are headed to the main server, you may try to kill them off with 'blaster' or look in the 'secret chest'. ");
             System.out.print(">  ");
-            Look = keyboard.next();
+            Blast = keyboard.next();
 
-            if (Look.equalsIgnoreCase("blaster")) {
-                System.out.print("Please enter a javascript function that adds numbers 3 and 5 and returns the sum, do this in es6");
-                //request input from the user;
+            if (Blast.equalsIgnoreCase("blaster")) {
+                System.out.print(" Please enter a javascript function that adds numbers x and y and returns the sum can you perform attack 'yes' or 'no'.");
+                Test = keyboard.next();
+            if(Test.equalsIgnoreCase("yes"))
+
+
+                System.out.println("Enter javaScript function called addition(x, y): returns the sum ");
+                String var1 = keyboard.next();
+
+                System.out.println("Enter second word: ");
+//                String var2 = Scanner.nextLine();
+
+//                if (same (var1, var2))
+//                    System.out.println("Yes");
+//                else
+//                    System.out.println("No");
+//            }
+
+//            public static boolean same (String var1, String var2){
+//                if (var1 == var2)
+//                    return true;
+//                else
+//                    return false;
+//            }
 
                 System.out.println("After killing off some of the bugs, you have used the battery life and need to wait for it to recharge. Would you like to eat some of the food, 'Yes' or 'No'?");
                 System.out.print(">  ");
@@ -41,7 +63,7 @@ public class HighLow {
                     System.out.println("  ");
                     System.out.println("You die of starvation!");
                 }
-            } else if (Look.equalsIgnoreCase("pantry")) {
+            } else if (Blast.equalsIgnoreCase("pantry")) {
                 System.out.println("There is a killer inside. Do you want to 'fight' them, or 'run away'?");
                 System.out.print(">  ");
                 Pantry = keyboard.next();
