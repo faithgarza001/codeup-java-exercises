7284e89077dda70aac2cb1548d7d3e643b02cc6b
faithgarza <faith.garza001@gmail.com>
2020-02-03 11:57:16 -0600

    progress

---
+++ src/ArraysExercises.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+
+    String name;
+    String language;
+    String origin;
+/*Create an array that holds 3 person objects*/
+    public static void main(String[] args) {
+/*Assign a new instance of the person class to each element.*/
+        String [] peoples = {"Jim", "Jones", "Jill", "Jane"};
+        /*Iterate through the array and print out the name of each person in the array*/
+        for (String people : peoples) {
+            System.out.println(people);//this for loop will print all  the names of the array perhaps we can go back and create an enhance for loop*/
+        }
+        //System.out.print(peoples.length);//this only prints out the length of the array
+    ArraysExercises Faith = new ArraysExercises();
+            Faith.name = "Faith";
+            Faith.language = "JavaScript";
+            Faith.origin = "San Antonio";
+            System.out.print(Faith.fullReport());
+    }
+/*Create a static method named addPerson. It should accept an array of Person objects, as well as a single person object to add to the passed array. It should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array*/
+    public String fullReport() {
+        String stuff = "";
+        stuff += "My name is " + this.name;
+        stuff += " I started learning " + this.language + " I live in " + this.origin;
+        return stuff;
+    }
+
+
+
+
+
+    }
+
+
+
