839412acb20e15b4cb2483381a1012a45bcfdfd6
faithgarza <faith.garza001@gmail.com>
2020-02-03 13:57:07 -0600

    progress with exercise 2

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,19 @@
+import java.util.Arrays;
+
 public class ServerNameGenerator {
-}
+/*We are going to build a server name generator. Create a class inside of src named ServerNameGenerator, and follow the specs below.*/
+
+    public static void main(String[] args){
+/*Create two arrays whose elements are strings: one with at least 10 adjectives, another with at least 10 nouns.*/
+        String [][] movies = {
+                {"suspenseful", "absorbing","uproarious","intriguing", "first-rate", "tender", "original", "riveting", "dazzling", "legendary"},
+                {"silent movie","cartoon", "multiplex", "director", "producer", "star", "trailer", "soundtrack", "credits"},
+
+        };
+
+    }
+
+   /* Create a method that will return a random element from an array of strings.*/
+
+
+        }
