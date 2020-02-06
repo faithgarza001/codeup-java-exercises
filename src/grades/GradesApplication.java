bc90f3115ddf7293c2b21aae0fe1f1f09fd32812
faithgarza <faith.garza001@gmail.com>
2020-02-06 15:58:22 -0600

    completed

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,18 +1,16 @@
 package grades;
-
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class GradesApplication {
 
+
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 //Here you are defining the hash map
         /*It should have keys that are strings that represent github usernames, and values that are Student objects. Create at least 4 student objects with at least 3 grades each, and add them to the map*/
-        HashMap<String, String> students = new HashMap<>();
-        //now you are putting data into the hash map
-        students.put("Jill", "blackcat23");//key value pairs
-        students.put("Faith", "Fey123");
-        students.put("Jane", "Jannette001");
-        students.put("Joe", "Joseph68");
+
 
 
         Student student = new Student("Jill");//Object instances of each student
@@ -35,7 +33,50 @@ public class GradesApplication {
         student3.addGrade(80);
         student3.addGrade(70);
 
+        HashMap<String, Student> students = new HashMap<>();
+        //now you are putting data into the hash map must be at the bottom so that your Hash map can read the objects
+        students.put("blackcat23",student);//key value pairs
+        students.put("Faith", student1);
+        students.put("Jane", student2);
+        students.put("Joe", student3);
 
-        System.out.println(students);
-    }
+        System.out.println("Welcome!!\n Here are the  Github usernames of our students\n");
+        students.forEach((key, value) -> System.out.println(key));//Java function with a forEach loop this is protocol for HashMap!!!Do not use this unless it's Hash
+
+//        System.out.println(students);
+
+
+
+        /*Print the list of GitHub usernames out to the console, and ask the user which student they would like to see more information about. The user should enter a GitHub username (i.e. one of the keys in your map). If the given input does not match up with a key in your map, tell the user that no users with that username were found. If the given username does exist, display information about that student, including their name and their grades*/
+        String userInput = "";
+        int userContinue = 1;
+        //outer do gives information inner do tells what to do with data if it not correct.
+
+      do {
+
+
+          do {
+              if (userInput != ""){
+                  System.out.println("Sorry, no student found with the GitHub username of ");
+              }
+                System.out.println("What student would you like to see more information on?\n");
+                userInput = scanner.next();
+
+          } while (!students.containsKey(userInput));
+          //function for displaying data after the while loop has processed(after userInput has been taken in
+
+          System.out.println(students.get(userInput).name() + "\n" + students.get(userInput).getGradeAverage());
+
+
+          System.out.println("Do you want to continue? 1:yes or 2:no");
+          userContinue = scanner.nextInt();
+          userInput = "";
+
+
+      }while (userContinue == 1);
+
+}
+
+
+
 }
