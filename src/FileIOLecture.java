90f3e609f056eb89574ee7d3297950e01f564adc
faithgarza <faith.garza001@gmail.com>
2020-02-10 10:45:31 -0600

    progress

---
+++ src/FileIOLecture.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileIOLecture {
+
+    public static void main(String[] args) {
+        newFile();
+        writeFile();
+    }
+    static void newFile(){
+        String directory = "data";
+        String filename = "info.txt";
+
+        Path dataDirectory = Paths.get(directory);
+        System.out.println(dataDirectory.toAbsolutePath());
+        Path dataFile = Paths.get(directory, filename);
+
+
+
+        try{
+            if(Files.notExists(dataDirectory)) {
+                Files.createDirectories(dataDirectory);
+                System.out.println("Created Directory");
+            }
+            if(!Files.exists(dataFile)) {
+                Files.createFile(dataFile);
+                System.out.println("Created file");
+            }
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+
+    }
+ //program to interfaces versus implementation
+    static void writeFile() {
+        List<String> fruit = new ArrayList<>();
+        fruit.add("banana");
+        fruit.add("orange");
+        for (String fr : fruit)
+            System.out.println(fr);
+        try {
+            Path groceries = Paths.get("data", "groceries.txt");
+            Files.write(groceries, fruit);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+
+}
---
+++ src/pushup.java
@@ -0,0 +1,45 @@
+//import java.io.File;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import java.util.ArrayList;
+//import java.util.Scanner;
+//
+public class pushup {
+//    private static Scanner scanner = new Scanner(System.in);
+//    private static GroceryList = new GroceryList();
+//    /* write a program that gives a list of strings and you arte able to add ontot the list*/
+//    public static void main(String[] args) {
+//        boolean quit = false; //this line says program quit while false
+//        int choice = 0;
+//        printInstructions();
+//
+//        while(!quit){
+//            System.out.println("Enter your choice");
+//            choice = scanner.nextInt();
+//            scanner.nextLine();
+//
+//            switch(choice){
+//                case 0:
+//                    printInstructions();
+//                    break;
+//
+//                case 1:
+//                    Gro
+//
+//            }
+//        }
+//
+//
+//
+//    }
+//
+//    private static void printInstructions() {
+//
+//    }
+//
+//
+}
+
+
+
